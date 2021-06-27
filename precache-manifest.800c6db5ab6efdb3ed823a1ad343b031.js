053fc43b1623846885a15aaa0b3b401d4de9c7f6
mohitjayant <mkumar5@ce.iitr.ac.in>
2021-06-27 18:22:10 +0530

    Updates

---
+++ precache-manifest.800c6db5ab6efdb3ed823a1ad343b031.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "161f2d6de8e3803806959a4ac7dbae3a",
+    "url": "/mohit-dev-portfolio/index.html"
+  },
+  {
+    "revision": "83e53a978e4a748fc028",
+    "url": "/mohit-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "a995ef2fc2c01f6d47b8",
+    "url": "/mohit-dev-portfolio/static/css/main.40f0e6cc.chunk.css"
+  },
+  {
+    "revision": "83e53a978e4a748fc028",
+    "url": "/mohit-dev-portfolio/static/js/2.26ad827e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/mohit-dev-portfolio/static/js/2.26ad827e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a995ef2fc2c01f6d47b8",
+    "url": "/mohit-dev-portfolio/static/js/main.7ed0fd5c.chunk.js"
+  },
+  {
+    "revision": "aa2ad68b5bfcf2179418",
+    "url": "/mohit-dev-portfolio/static/js/runtime-main.687d0e7f.js"
+  }
+]);
